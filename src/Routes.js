a01521a918c150ea364174b06a61b84b58e949e0
Charles <charles@MacBook.local>
2017-07-13 03:05:33 +0900

    Criando Componente Boas Vindas

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import FormLogin from './components/FormLogin';
 import FormCadastro from './components/FormCadastro';
+import BoasVindas from './components/BoasVindas';
 
 class Routes extends Component {
   render() {
@@ -10,6 +11,7 @@ class Routes extends Component {
         <Scene key='root'>
           <Scene key='formLogin' component={FormLogin} title='Login' />
           <Scene key='formCadastro' component={FormCadastro} title='Cadastro' />
+          <Scene key='boasVindas' component={BoasVindas} title='Boas Vindas' initial />
         </Scene>
       </Router>
     );
---
+++ src/components/BoasVindas.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { ImageBackground, Image } from 'react-native';
+import { Card, CardSection, Header, Button } from './common';
+
+const bg = require('../imgs/bg.png');
+const logo = require('../imgs/logo.png');
+
+class BoasVindas extends Component {
+  render() {
+    return (
+      <ImageBackground style={{ flex: 1 }} source={bg}>
+        <Card>
+          <Header 
+            style={{ flex: 1 }}
+            headerText='Seja Bem Vindo' 
+          />
+          <CardSection
+          style={{ flexDirection: 'column', flex: 2, alignItems: 'center' }}
+          >
+            <Image source={logo} />
+          </CardSection>
+          <CardSection
+          style={{ flexDirection: 'column', flex: 2, alignItems: 'center' }}
+          >
+            <Button onPress={() => false}>
+              Fazer Login
+            </Button>
+          </CardSection>
+        </Card>
+      </ImageBackground>
+    );
+  }
+}
+
+export default BoasVindas;
+// parei na aula 224...
