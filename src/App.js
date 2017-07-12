a94b3d87a31507f2698dcb2655f1b7dc839dce99
Charles <charles@MacBook.local>
2017-07-12 18:31:06 +0900

    Commit instalação react-redux redux CreatStore Provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { creatStore } from 'redux';
 import Routes from './Routes';
 
 class App extends Component {
   render() {
     return (
-      <Routes />
+      <Provider store={creatStore(reducers)}>
+        <Routes />
+      </Provider>
     );
   }
 }
