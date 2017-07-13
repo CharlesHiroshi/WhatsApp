7a1766380f3e566a86850c9c4b21a71859ca6bbd
Charles <charles@MacBook.local>
2017-07-13 15:38:16 +0900

    Criando o Componente Menu

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import { ImageBackground, StyleSheet, View } from 'react-native';
-import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
-import { Card, Header } from './common';
+import { TabViewAnimated, SceneMap } from 'react-native-tab-view';
+import { Card } from './common';
+import TabBarMenu from './TabBarMenu';
 
-const FirstRoute = () => <View style={[styles.container, { backgroundColor: '#ff4081' }]} />;
-const SecondRoute = () => <View style={[styles.container, { backgroundColor: '#673ab7' }]} />;
+const Conversas = () => <View style={[styles.container, { backgroundColor: '#ff4081' }]} />;
+const Contatos = () => <View style={[styles.container, { backgroundColor: '#673ab7' }]} />;
 
 export default class Principal extends Component {
   state = {
     index: 0,
     routes: [
-      { key: '1', title: 'First' },
-      { key: '2', title: 'Second' },
+      { key: '1', title: 'Conversas' },
+      { key: '2', title: 'Contatos' },
     ],
   };
 
   _handleChangeTab = index => this.setState({ index });
 
-  _renderHeader = props => <TabBar {...props} />;
+  _renderHeader = props => <TabBarMenu {...props} />;
 
   _renderScene = SceneMap({
-    1: FirstRoute,
-    2: SecondRoute,
+    1: Conversas,
+    2: Contatos,
   });
 
   render() {
