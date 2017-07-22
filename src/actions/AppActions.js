a2a02efc9d4f0c899dbf892fe7e128ce82a13097
Charles <charles@MacBook.local>
2017-07-23 02:16:00 +0900

    Adicionando contatos do usuário parte 2

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,16 +1,27 @@
+import b64 from 'base-64';
 import firebase from 'firebase';
 import { MODIFICA_ADICIONA_CONTATO_EMAIL } from './types';
 
-export const modificaAdicionaContatoEmail = texto => {
-  return {
+export const modificaAdicionaContatoEmail = texto => ({
     type: MODIFICA_ADICIONA_CONTATO_EMAIL,
     payload: texto
-  };
-};
+  });
 
 export const adicionaContato = email => {
-  console.log(email);
+  const emailB64 = b64.encode(email);
+  firebase.database().ref(`/contatos/${emailB64}`)
+    .once('value')
+    .then(snapshot => {
+      console.log(snapshot.val());
+      if (snapshot.val()) {
+        console.log('Usuário existe');
+      } else {
+        console.log('Usuário não existe');
+      }
+    });
   return {
     type: ''
   };
 };
+
+// Aula 250 - Adicionando contatos do usuário parte 2
