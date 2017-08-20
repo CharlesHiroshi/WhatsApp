5d2e512f47896ba228d4afb8bcfcead9fcdebc8e
Charles <charles@MacBook.local>
2017-08-20 18:26:39 +0900

    Listando conversas - Parte 5 - Exibindo Conversas

--- src/components/Conversas.js
+++ src/components/Conversas.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { ImageBackground } from 'react-native';
+import { 
+  ImageBackground, 
+  ListView, 
+  View, 
+  Text, 
+  TouchableHighlight 
+} from 'react-native';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import { Card } from './common';
@@ -10,17 +16,57 @@ const bg = require('../imgs/bg.png');
 class Conversas extends Component {
   componentWillMount() {
     this.props.conversasUsuarioFetch();
+    this.criaFonteDeDados(this.props.conversas);
     console.log('componentWillMount: ', this.props.conversas);
   }
 
   componentWillReceiveProps(nextProps) {
+    this.criaFonteDeDados(nextProps.conversas);
     console.log('componentWillReceiveProps: ', nextProps.conversas);
+  }
+
+  criaFonteDeDados(conversas) {
+    const ds = new ListView.DataSource({ 
+      rowHasChanged: (r1, r2) => r1 !== r2 
+    });
+    this.dataSource = ds.cloneWithRows(conversas);
+  }
+
+  renderRow(conversa) {
+    return (
+      <View><Text>{conversa.nome}</Text></View>
+      // <TouchableHighlight
+      //   underlayColor='transparent'
+      //   onPress={() => Actions.conversa({ 
+      //     title: contato.nome,
+      //     contatoNome: contato.nome, 
+      //     contatoEmail: contato.email 
+      //   })}
+      // >
+      //   <View
+      //     style={{ 
+      //     flex: 1, 
+      //     padding: 20, 
+      //     borderBottomWidth: 1, 
+      //     borderColor: '#CCC' }}
+      //   >
+      //     <Text style={{ fontSize: 25 }} >{contato.nome}</Text>
+      //     <Text style={{ fontSize: 18 }} >{contato.email}</Text>
+      //   </View>
+      // </TouchableHighlight>
+    );
   }
 
   render() {
     return (
       <ImageBackground style={{ flex: 1 }} source={bg}>
-        <Card />
+        <Card>
+          <ListView 
+            enableEmptySections
+            dataSource={this.dataSource}
+            renderRow={this.renderRow}
+          />
+        </Card>
       </ImageBackground>
     );
   }
@@ -37,5 +83,5 @@ const mapStateToProps = state => {
 
 export default connect(mapStateToProps, { conversasUsuarioFetch })(Conversas);
 
-// Aula 287
-// Listando conversas - Parte 4 - Recuperando Conversas
+// Aula 288
+// Listando conversas - Parte 5 - Exibindo Conversas
